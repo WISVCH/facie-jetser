d4e81f1102a4e165db5be8fae0f8aa87a9efc002
Francis Behnen <francis.behnen@gmail.com>
2018-09-26 21:47:50 +0200

    Fix databaseconfig and connection

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 //	$mysqli = mysqli_connect("localhost.mysql", "root", "", "facie_jetser");
 //	die(mysqli_error($mysqli));
 
-    $servername = "127.0.0.1";
-    $username = "facieUser";
+    $servername = "facie-jetser-stack_facie-db";
+    $username = "root";
     $password = "faciePassword";
     $database = "facie_jetser";
     $port     =  3306;
